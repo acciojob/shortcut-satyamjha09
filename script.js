b712a97efe6e406733c6bfff573a8dde08121ea6
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-12-01 21:16:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function shortcut(s1, s2) {
-  // your code here
-}
+     
+	if(s1.length == 0 || s2.length == 0) {
+		return ''; 
+	} else {
+		return s1[0].concat(s2[0]);
+	}
 
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
+  
+}
+ 
+
+const s1 = prompt("Enter s1:");   
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
